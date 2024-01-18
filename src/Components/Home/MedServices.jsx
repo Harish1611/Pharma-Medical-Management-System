9abf4f4415404748491d0606488d04e45795d0cd
Harish1611 <harishnaidub005@gmail.com>
2024-01-18 13:54:25 +0530

    New: TailwindCSS, Home, Footer & Nav Components created

---
+++ src/Components/Home/MedServices.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MedServices = () => {
+  return (
+    <div>MedServices</div>
+  )
+}
+
+export default MedServices
---
+++ src/Components/Home/WelcomBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WelcomBanner = () => {
+  return (
+    <div>WelcomBanner</div>
+  )
+}
+
+export default WelcomBanner
