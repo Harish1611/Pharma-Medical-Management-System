4f9dbcbe5ad5a1196b9ba30d702064859a0aa87b
Harish1611 <harishnaidub005@gmail.com>
2024-02-21 23:06:20 +0530

    New: Mobile Weclome Banner Carousel Images added

--- src/Components/Home/WelcomBannerData.js
+++ src/Components/Home/WelcomBannerData.js
@@ -2,6 +2,11 @@ import SliderImage1 from '../../Assets/Slider-Images/2.png'
 import SliderImage2 from '../../Assets/Slider-Images/1.png'
 import SliderImage3 from '../../Assets/Slider-Images/3.png'
 
+import MobileSliderImage1 from '../../Assets/Mobile-Slider-Images/2.png'
+import MobileSliderImage2 from '../../Assets/Mobile-Slider-Images/1.png'
+import MobileSliderImage3 from '../../Assets/Mobile-Slider-Images/3.png'
+
+
 
 export const WelcomBannerData = [
   {
@@ -20,15 +25,13 @@ export const WelcomBannerData = [
 
 export const WelcomBannerMobileData = [
   {
-    image: "https://placehold.co/600x400",
+    image:MobileSliderImage1,
   },
   {
-    image: "https://placehold.co/600x400",
+    image:MobileSliderImage2,
   },
   {
-    image: "https://placehold.co/600x400",
+    image:MobileSliderImage3,
   },
-  {
-    image: "https://placehold.co/600x400",
-  },
+ 
 ];
