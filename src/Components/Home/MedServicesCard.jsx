8d4ce6103c0fbdc62bfc51e9ef4e3d7deb423bc8
Harish1611 <harishnaidub005@gmail.com>
2024-02-21 22:13:56 +0530

    New: MedService navigation is added to cards using useNavigation Hook

--- src/Components/Home/MedServicesCard.jsx
+++ src/Components/Home/MedServicesCard.jsx
@@ -3,16 +3,20 @@ import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturi
 import PublishedWithChangesIcon from '@mui/icons-material/PublishedWithChanges';
 import SummarizeIcon from '@mui/icons-material/Summarize';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import { useNavigate } from "react-router-dom";
+
 
 import { Grid } from '@mui/material';
 
 
 const MedServicesCard = ({ cardData }) => {
+  const navigate = useNavigate();
+
   return (
     <Grid container spacing={2}  >
       {cardData.map((item, index) => (
         <Grid key={index} item xs={12} sm={6} >
-          <div className='border rounded-md py-4 px-2 sm:p-4  hover:shadow-md cursor-pointer flex overflow-hidden'>
+          <div className='border rounded-md py-4 px-2 sm:p-4  hover:shadow-md cursor-pointer flex overflow-hidden' onClick={() => navigate(`/${item.url}`)} >
             <div className='flex-col'>
               {item.icon} {/* Render the icon component */}
             </div>
--- src/Components/Home/MedServicesData.js
+++ src/Components/Home/MedServicesData.js
@@ -7,17 +7,24 @@ export const MedServicesData = [
   {
     service: "PMMS Batch Processing",
     icon: <PublishedWithChangesIcon />,
+    url:'batch-processing',
   },
   {
     service: "PMMS Report Generation",
     icon: <SummarizeIcon />,
+    url:'report-generation',
+
   },
   {
     service: "PMMS Manufacturing",
     icon: <PrecisionManufacturingIcon />,
+    url:'manufacturing',
+
   },
   {
     service: "PMMS Packaging & Shipping",
     icon: <LocalShippingIcon />,
+    url:'packaging-shipping',
+
   },
 ];
