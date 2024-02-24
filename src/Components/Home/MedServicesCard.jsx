9c64eac7248d90a0446ef075b427886a46693008
Harish1611 <harishnaidub005@gmail.com>
2024-02-24 21:16:32 +0530

    Modified: Updated the Med Services Style for mobile

--- src/Components/Home/MedServicesCard.jsx
+++ src/Components/Home/MedServicesCard.jsx
@@ -21,7 +21,7 @@ const MedServicesCard = ({ cardData }) => {
               {item.icon} {/* Render the icon component */}
             </div>
             <div className='flex-col'>
-              <p class='text-slate-800 font-semibold text-lg sm:text-lg hover:text-slate-500 px-3 sm:px-8'>{item.service}</p> {/* Render the service name */}
+              <p class='text-slate-800 font-normal text-lg sm:text-lg hover:text-slate-500 px-3 sm:px-8'>{item.service}</p> {/* Render the service name */}
             </div>
           </div>
         </Grid>
