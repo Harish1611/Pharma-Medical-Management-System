85f1febbfd32ec69790b59cbc8f5c15c4cc976de
Harish1611 <harishnaidub005@gmail.com>
2024-02-25 23:41:49 +0530

    Modified: New Navigation Link is added to the Your Profile menu item in Nav Component

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -104,24 +104,17 @@ export default function Nav() {
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          <div
+                            
+                            onClick={()=>{navigate('/profile')}}
+                            className='cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
+
                           >
                             Your Profile
-                          </a>
+                          </div>
                         )}
                       </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
+                      
                       <Menu.Item>
                         {({ active }) => (
                           <a
