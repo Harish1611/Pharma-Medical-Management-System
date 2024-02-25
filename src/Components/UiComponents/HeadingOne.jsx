b6fd50287ec6e66d01a2f2af548dbac8cd7639b6
Harish1611 <harishnaidub005@gmail.com>
2024-02-25 23:29:37 +0530

    Modified: HeadingOne Component mobile Style Modified

--- src/Components/UiComponents/HeadingOne.jsx
+++ src/Components/UiComponents/HeadingOne.jsx
@@ -5,7 +5,7 @@ const HeadingOne = (props) => {
     <div className=''>
 
         <div className='pl-10 text-left'>
-            <h2 className=' font-bold font-cinzel text-2xl pt-8 pb-2  '> {props.headText} </h2>
+            <h2 className=' font-bold font-cinzel text-lg sm:text-2xl pt-8 pb-2  '> {props.headText} </h2>
             <hr class="w-48 h-1  bg-[#FFDE59] border-0 rounded " />
 
         </div>
