b145e5b17ba37ac74beea03aaab06533564d0009
Harish1611 <harishnaidub005@gmail.com>
2024-03-06 19:30:58 +0530

    New: Create New Resource Sample Form and Component is created with responsive design

--- src/Components/RawMaterialResources/NewRawMaterial.jsx
+++ src/Components/RawMaterialResources/NewRawMaterial.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import HeadingOne from '../UiComponents/HeadingOne';
+import { Grid } from '@mui/material';
+import { GroupAdd } from '@mui/icons-material';
 
 const NewRawMaterial = () => {
   return (
-    <div>
+
+    <div className='mt-4'>
 
          <HeadingOne headText="Create New Resources" />
-         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+         <form class="bg-white px-2 py-2 m-2 sm:px-20 sm:py-10 sm:m-14 border-slate-200 rounded-md shadow-md">
 
-         <div>
+          <Grid container spacing={2}>
+
+            <Grid item sm={4} xs={12}>
          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Resource Name" />
-         </div>
-
+         </Grid>
+         <Grid item sm={4} xs={12} >
+         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Resource Type" />
+         </Grid>
+         <Grid item sm={4} xs={12} >
+         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Requested By" />
+         </Grid>
+         
+         <Grid item xs={8}>
          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
         
-      Create
-      </button>
-         </form>
+         Create
+         </button>
+         </Grid>
+
+         </Grid>
+      </form>
 
     </div>
   )
