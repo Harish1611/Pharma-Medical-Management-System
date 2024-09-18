1913541e24824c432bdac42f9f4b153f6e6aca13
Harish1611 <harishnaidub005@gmail.com>
2024-09-18 21:04:11 +0530

    New: Created StartBatch, OnGoingBatches & CompletedBatches Component Structure

---
+++ src/Components/BatchProcessing/OnGoingBatches/OnGoingBatches.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OnGoingBatches = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default OnGoingBatches;
---
+++ src/Components/BatchProcessing/StartBatch/StartBatch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StartBatch = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default StartBatch;
