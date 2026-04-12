4d3d9b161730688c9e343ad921febf9f54e1c631
Harish1611 <harishnaidub005@gmail.com>
2026-04-12 12:23:19 +0530

    New: Implement Profile section layout with image, name & designation

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div>Profile</div>
+
+    <div className="w-full p-5">
+    <div className="flex items-center gap-4 p-4 border rounded-lg shadow-sm  ">
+      
+      {/* Profile Image */}
+      <img
+        src="https://i.pravatar.cc/100"
+        alt="profile"
+        className="w-16 h-16 rounded-full object-cover"
+      />
+
+      {/* Name + Role */}
+      <div>
+        <h2 className="text-lg font-semibold text-gray-800">
+          Harish Naidu
+        </h2>
+        <p className="text-sm text-gray-500">
+          MERN Stack Developer
+        </p>
+      </div>
+
+    </div>
+    </div>
   )
 }
 
